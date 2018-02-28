0b0c780160448b39b9245aec36378c17392dd117
unknown <Andrei Auchinnikau>
2018-02-28 23:23:43 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  var max = 0, i = 0;
+  var hashSet = new Set();
+
+  for (i = 0; i<array.length; i++){
+    hashSet.add(array[i]);
+  }
+
+  for (i = 0; i < array.length; i++) {
+        if (!hashSet.has(array[i]-1))
+        {
+            var j = array[i];
+            while (hashSet.has(j)){
+                j++;
+            }
+            if (max<j-array[i]) max = j-array[i];
+        }
+    
+  }
+
+  return max;
 }
